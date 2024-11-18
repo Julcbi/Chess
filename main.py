ebd7cec84dcb1710f49fb82455f2b199bee688bd
Julcbi <juliacbindi@gmail.com>
2024-11-18 17:57:34 +0100

    final version 18/11
    move of W and B at the rigth time

--- main.py
+++ main.py
@@ -137,14 +137,27 @@ class ChessBoard:
             [Rook("W"), Horse("W"), Bishop("W"), Queen("W"), King("W"), Bishop("W"), Horse("W"), Rook("W")],
         ]
 
+    def step(self, is_White, src, dst):
+        f = self.board[int(src[0]) - 1][ord(src[1]) - 97]
+
+        if (f.color == "B" and is_White) or (f.color == "W" and not is_White):
+            print("You are not using your figure")
+            return False
+
+        self.board[int(dst[0]) - 1][ord(dst[1]) - 97] = self.board[int(src[0]) - 1][ord(src[1]) - 97]
+        self.board[int(src[0]) - 1][ord(src[1]) - 97] = None
+
+        return True
+
     def render(self):
         print("   a  b  c  d  e  f  g  h\n")
         for (i, r) in enumerate(self.board):
             print(i + 1, end="  ")
             for f in r:
                 if f == None:
-                    print(end=" ")
+                    print(end="   ")
                 else:
+                    # TODO: Color the output
                     print(f.letter(), end=" ")
             print()
 
@@ -153,6 +166,25 @@ class ChessBoard:
 
 
 board = ChessBoard()
-board.render()
+
+is_White = True 
 
  
+while True:
+    board.render()
+    if is_White: 
+        print("White turn")
+    else:
+        print("Black turn")
+    src = input ("src: ")
+    dst = input ("dst: ")
+
+    while not board.step(is_White, src, dst):
+        print("Wrong input, try again")
+        src = input ("src: ")
+        dst = input ("dst: ")
+
+    is_White = not is_White
+
+
+    
